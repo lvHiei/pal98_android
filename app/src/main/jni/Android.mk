a6aac90ace28a472a477fce7783614c2bb04c852
majun <majun@mail.51vv.com>
2017-01-12 15:27:30 +0800

    init my pal98

---
+++ app/src/main/jni/Android.mk
@@ -0,0 +1,5 @@
+# The namespace in Java file, with dots replaced with underscores
+SDL_JAVA_PACKAGE_PATH    := com_codeplex_sdlpal
+SDL_ANDROID_PACKAGE_NAME := com.codeplex.sdlpal
+
+include $(call all-subdir-makefiles)
