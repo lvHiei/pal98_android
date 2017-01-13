df9867201ced9c5afb41401d3a75a19bbc0390e3
majun <majun@mail.51vv.com>
2017-01-13 14:23:29 +0800

    add log macro

--- app/src/main/jni/app_sdlpal/common.h
+++ app/src/main/jni/app_sdlpal/common.h
@@ -29,6 +29,14 @@ extern "C"
 {
 #endif
 
+#include <android/log.h>
+#define TAG "mj_test"
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,TAG ,__VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG ,__VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,TAG ,__VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG ,__VA_ARGS__)
+#define LOGF(...) __android_log_print(ANDROID_LOG_FATAL,TAG ,__VA_ARGS__)
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
